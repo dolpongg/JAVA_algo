9bff5f207a780f5c6d6bb532e0ffc8b048c9d26f
dolpongg <dolpong_@naver.com>
2022-08-24 23:53:57 +0900

    0822 commit

--- 백준/Gold5_5014_스타트링크.java
+++ 백준/Gold5_5014_스타트링크.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Gold5_5014_스타트링크 {
@@ -15,15 +17,33 @@ public class Gold5_5014_스타트링크 {
 		int U = Integer.parseInt(st.nextToken());
 		int D = Integer.parseInt(st.nextToken());
 
-		if(G > S) {
-			int gap = G-S;
-			if(gap % U == 0) {
-				System.out.println(gap / U);
-			}else {
-				if(gap / U)
-			}
-		}
+		boolean [] visited = new boolean[F+1];
 		
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {S,0});
+		visited[S] = true;
+		
+		while(!q.isEmpty()) {
+			int [] current = q.poll();
+			
+			if(current[0] == G) {
+				System.out.println(current[1]);
+				return;
+			}
+			
+			int nFloor = current[0]+U;
+			if(nFloor < F && !visited[nFloor]) {
+				visited[nFloor] = true;
+				q.offer(new int[] {nFloor, current[1]+1});
+			}
+			nFloor = current[0] - D;
+			if(nFloor >= 0 &&!visited[nFloor]) {
+				visited[nFloor] = true;
+				q.offer(new int[] {nFloor, current[1]+1});
+			}
+			
+		}
+		System.out.println("use the stairs");
 	}
 
 }
---
+++ 백준/Silver_1326_폴짝폴짝.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Silver_1326_폴짝폴짝 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());//징검다리 수 
+		int [] stones = new int [N+1]; 
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <N+1; i++) {
+			stones[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken()); //개구리 있는 곳 
+		int b = Integer.parseInt(st.nextToken()); //가고 싶은 곳  
+		//입력 완료
+
+		boolean[] visited = new boolean[b+1]; 
+		Queue<int[]> q = new LinkedList<>();
+		q.offer(new int[] {a,1});
+		
+		while(!q.isEmpty()) {
+			int[] current = q.poll();
+			
+			if(current[0] == b) {
+				System.out.println(current[1]-1);
+				return;
+			}
+			if((b - current[0]) % stones[current[0]] == 0) {
+				System.out.println(current[1]);
+				return;
+			}
+			
+			int nStone = current[0] + stones[current[0]];
+			while(nStone <= b) {
+				if(!visited[nStone]) {
+					visited[nStone] = true;
+					q.offer(new int[] {nStone,current[1]+1});
+					nStone+= stones[current[0]];
+				}
+			}
+			
+			
+		}
+		System.out.println(-1);
+		
+		
+	}
+}
+
+
+/*
+5
+1 2 2 1 2
+1 5
+*/
+
